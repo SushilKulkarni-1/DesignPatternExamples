5bf604147bc51e25c12b48b92365de0ead93e0cc
sushilbk2007 <sushilbk2007@gmail.com>
2020-08-26 22:06:05 +0530

    Revert "Price Changed"
    This reverts commit 459a562dc3901ddc519ff1e0b9469ae37682dd02.

--- DesignPatternExamples/Decorator/ConcreteDecorator/SunRoofCarDecorator.cs
+++ DesignPatternExamples/Decorator/ConcreteDecorator/SunRoofCarDecorator.cs
@@ -12,7 +12,7 @@
 
         public override string GetDescription() => _car.GetDescription() + "," + _description;
 
-        public override double GetPrice() => _car.GetPrice() + 2500;
+        public override double GetPrice() => _car.GetPrice() + 7000;
     }
 
 
