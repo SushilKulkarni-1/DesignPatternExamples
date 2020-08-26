b20fdb2ff9be5ecac9e1197f3efc5a21cee18674
sushilbk2007 <sushilbk2007@gmail.com>
2020-08-26 22:24:16 +0530

    Revert "Glass sunroof"
    This reverts commit 264d19c0a7232bf4633d6fbe1c502cbc177caf5d.

--- DesignPatternExamples/Decorator/ConcreteDecorator/SunRoofCarDecorator.cs
+++ DesignPatternExamples/Decorator/ConcreteDecorator/SunRoofCarDecorator.cs
@@ -7,7 +7,7 @@
         public SunRoofCarDecorator(ICar car) : base(car)
         {
             _car = car;
-            _description = "Glass SunRoof  ";
+            _description = "SunRoof ";
         }
 
         public override string GetDescription() => _car.GetDescription() + "," + _description;
